d245315face186c9c79611b101a063a22be668c8
Pablo Andrade <pabllo007@gmail.com>
2023-04-05 20:01:33 -0300

    RestTemplate

--- hr-worker/src/main/java/br/com/pabllo007/hrworker/resources/WorkerResource.java
+++ hr-worker/src/main/java/br/com/pabllo007/hrworker/resources/WorkerResource.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@RequestMapping(value = "/works")
+@RequestMapping(value = "/workers")
 public class WorkerResource {
 
     @Autowired
