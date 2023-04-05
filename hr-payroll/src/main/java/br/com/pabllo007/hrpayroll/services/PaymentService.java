c1acd41be13252bf06c2692ee532672042bf2a9e
Pablo Andrade <pabllo007@gmail.com>
2023-04-05 19:44:37 -0300

    hr-payroll implementando mock

---
+++ hr-payroll/src/main/java/br/com/pabllo007/hrpayroll/services/PaymentService.java
@@ -0,0 +1,12 @@
+package br.com.pabllo007.hrpayroll.services;
+
+import br.com.pabllo007.hrpayroll.entities.Payment;
+import org.springframework.stereotype.Service;
+
+@Service
+public class PaymentService {
+
+    public Payment getPayment(long workerId, int days) {
+        return new Payment("Bob", 200.0, days);
+    }
+}
