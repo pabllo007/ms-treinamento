7ccf4d3fdda03e910c09992cda70d2ec4c9ea5bf
Pablo Andrade <pabllo007@gmail.com>
2023-04-06 08:37:01 -0300

    Cria~ção do projeto config-server

---
+++ hr-config-server/src/test/java/br/com/pabllo007/hrconfigserver/HrConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.pabllo007.hrconfigserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
