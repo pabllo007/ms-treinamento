7aeb027aee4c4f466db1c5a40cc18fa3f9d6ae6f
Pablo Andrade <pabllo007@gmail.com>
2023-04-05 22:31:33 -0300

    Configuranco Eureka server

---
+++ hr-eureka-server/src/test/java/br/com/pabllo007/hrpayroll/HrEurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.pabllo007.hrpayroll;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrEurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
