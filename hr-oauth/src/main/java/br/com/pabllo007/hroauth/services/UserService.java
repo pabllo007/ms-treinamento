25bd4dec726664793c51165302891625edbc0b55
Pablo Andrade <pabllo007@gmail.com>
2023-04-07 23:29:36 -0300

    Configurando feignClient no hr-oauth

---
+++ hr-oauth/src/main/java/br/com/pabllo007/hroauth/services/UserService.java
@@ -0,0 +1,32 @@
+package br.com.pabllo007.hroauth.services;
+
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import br.com.pabllo007.hroauth.entities.User;
+import br.com.pabllo007.hroauth.feignclients.UserFeignClient;
+
+@Service
+public class UserService {
+
+	@Autowired
+	private static Logger logger = LoggerFactory.getLogger(UserService.class);
+	
+	@Autowired
+	private UserFeignClient userFeignClient;
+	
+	public User findByEmail(String email) {
+		
+		User user = userFeignClient.findByEmail(email).getBody();
+		if( user == null) {
+			logger.error("Email não encontrado = " + email);
+			throw new IllegalArgumentException("Email não encontrado");
+		}
+		logger.info("Email encontrado = " + email);
+		return user;
+	}
+	
+}
