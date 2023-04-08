62dea67b8b4587d10b20b43dc808a810f13c7c50
Pablo Andrade <pabllo007@gmail.com>
2023-04-07 22:35:25 -0300

    ReaÃlizando carga de dados

--- hr-user/src/main/java/br/com/pabllo007/hruser/entities/Role.java
+++ hr-user/src/main/java/br/com/pabllo007/hruser/entities/Role.java
@@ -3,7 +3,6 @@ package br.com.pabllo007.hruser.entities;
 import java.io.Serializable;
 import java.util.Objects;
 
-import javax.annotation.Generated;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
