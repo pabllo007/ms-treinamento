3d7a8431b26dfa5bcfb46f9a9947bd5be84c4e1b
Pablo Andrade <pabllo007@gmail.com>
2023-04-07 23:00:48 -0300

    Crialção do projeto Oauth

---
+++ hr-oauth/src/test/java/br/com/pabllo007/hroauth/HrOauthApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.pabllo007.hroauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrOauthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
